c1629dae01b6da726c13bd996cf9e876337a38ca
arceusVen1 <jeremy.venin@telecom-sudparis.eu>
2016-12-21 14:32:32 +0100

    first getters and setters documented

--- open_thermostat/plug.py
+++ open_thermostat/plug.py
@@ -2,6 +2,30 @@
 SETTINGS = {"probe": "", "type": "", "number": 0, "state": "off"}
 
 
+def _is_int(self, number):
+    """check if the type is a int or not
+
+    Args:
+        number (any): the number you want to test
+
+    Returns:
+        bool: True if number is a int
+    """
+    return isinstance(number, int)
+
+
+def _is_string(self, string):
+    """check if the type is a string or not
+
+    Args:
+        string (any): the string you want to check
+
+    Returns:
+        bool: True if string is really a string
+    """
+    return isinstance(string, str)
+
+
 class Plug():
     """deals with the electric plug Energenie and relay
     """
@@ -39,6 +63,8 @@ class Plug():
         Raises:
             ValueError: if the type differs from "energenie" or "relay"
         """
+        if not _is_string(type_):
+            raise TypeError("the type must be a string")
         if type_ != "energenie" or type_ != "relay":
             raise ValueError(
                 "the type should only be \"energenie\" or \"relay\"")
@@ -53,7 +79,16 @@ class Plug():
         pass
 
     def set_number(self, number):
-        if not isinstance(number, int):
+        """set the number of the pin or channel of the plug
+
+        Args:
+            number (Int): the pin or channel number
+
+        Raises:
+            TypeError: the number must be an integer
+            ValueError: the channel is between 1 and 4 and pin between 0 and 25
+        """
+        if not _is_int(number):
             raise TypeError("the channel or pin number should be an integer")
         if self.settings["state"] == "energenie" and number > 4 or number < 1:
             raise ValueError("the channel number should be between 1 and 4")
@@ -76,8 +111,11 @@ class Plug():
             state (String): "on" or "off"
 
         Raises:
-            ValueError: if tge state differs from "on" or "off"
+            TypeError: the state is a string
+            ValueError: if the state differs from "on" or "off"
         """
+        if not _is_string(state):
+            raise TypeError("the state must be a string of \"on\" or \"off\"")
         if state != "on" or state != "off":
             raise ValueError("the state should only be \"on\" or \"off\"")
         self.settings["state"] = state
