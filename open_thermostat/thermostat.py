322b833d2d810dd9c4f02d9c2b7b5622267b17fb
arceusVen1 <jeremy.venin@telecom-sudparis.eu>
2017-02-27 15:04:40 +0100

    debuging

--- open_thermostat/thermostat.py
+++ open_thermostat/thermostat.py
@@ -55,7 +55,7 @@ class Thermostat:
         for i in range(len(self.slugs)):
             fprobe = materials.get_ds18b20_by_slug(self.slugs[i])
             print(fprobe)
-            if fprobe:
+            if fprobe is not (None, None):
                 probe = Ds18b20(settings=fprobe[0])
                 if probe.has_config(materials) and probe.is_thermostated():
                     thermorange = probe.link_moment_value()
